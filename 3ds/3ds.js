afe1e59235d9dc5b95d2a6b2d7f7e4403a7445d2
Dwayne Slater <ds84182@gmail.com>
2014-03-19 08:09:53 -0400

    Generalize some code

--- 3ds/3ds.js
+++ 3ds/3ds.js
@@ -1,30 +1,3 @@
-var queryData = {}; //objects that point to datatype and id
-var dataNum = 0;
-function getDataProcessor(q)
-{
-	return function( data ) {
-		var csvobj = data;
-		for (var i in csvobj)
-		{
-			var v = csvobj[i];
-			if (v.local_language_id == 9)
-			{
-				var nam = v.name.toLowerCase();
-				if (queryData[nam] == null)
-				{
-					queryData[nam] = [];
-				}
-				queryData[nam].push({type:q,id:v[q+"_id"],csv:v});
-			}
-		}
-		dataNum--;
-		if (dataNum <= 0)
-		{
-			searchPage();
-		}
-	}
-}
-
 function searchPage()
 {
 	var match = function(qd)
@@ -65,31 +38,6 @@ $(function()
 	setTimeout(function()
 	{
 		window.scrollTo(0,215);
-		if (location.protocol != "file:")
-		{
-			//load files to do querys on
-			//use majik: $.csv.toObjects(data)
-			var queryFiles = [
-				"ability",
-				"item",
-				"location",
-				"move",
-				"pokemon_species",
-				"region",
-				"stat",
-				"version"
-			];
-			dataNum = queryFiles.length;
-			
-			for (var i in queryFiles)
-			{
-				var q = queryFiles[i];
-				$.get( "/json/"+q+"_names.json", getDataProcessor(q));
-			}
-		}
-		else
-		{
-			searchPage();
-		}
+		loadPP()
 	},500);
 });
