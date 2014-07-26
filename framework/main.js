5a51fba3c51a4652545cff84e49dfecc3305b61f
Dwayne Slater <ds84182@gmail.com>
2014-07-26 09:42:02 -0400

    Added fade animation to pages

--- framework/main.js
+++ framework/main.js
@@ -2,7 +2,7 @@ framework = {};
 
 document.addEventListener('click', function(e) {
 	var target = $(e.target);
-	if ( target.attr("class").indexOf("frameworklink") != -1 ) {
+	if ( e.target.className.indexOf("frameworklink") != -1 ) {
 		e.stopPropagation();
 		e.preventDefault();
 		framework.load(target.attr("href"));
@@ -11,11 +11,13 @@ document.addEventListener('click', function(e) {
 
 framework.load = function(page)
 {
-	$(document.body).animate({scrollTop: 0}, 500, function()
+	$(document.body).animate({scrollTop: 0}, 500);
+	
+	$("#maincontentghost").css("opacity",1).empty().append($("#maincontent").children().detach());
+	$("#maincontent").css("opacity",0).load("pages/"+page,function()
 	{
-		$(".maincontent").load("pages/"+page,function()
-		{
-		});
+		$("#maincontentghost").animate({opacity: 0}, 500, function(){$("#maincontentghost").empty();});
+		$("#maincontent").animate({opacity: 1}, 500);
 	});
 }
 
