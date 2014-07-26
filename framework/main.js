41d7c18686e8e2c0d21a41d40a0dd5e37e10d58e
Dwayne Slater <ds84182@gmail.com>
2014-07-26 02:53:28 -0400

    Framework hooks into all mouse events, if the clicked element is a framework link, it gets invoked

--- framework/main.js
+++ framework/main.js
@@ -1,8 +1,22 @@
 framework = {};
 
+document.addEventListener('click', function(e) {
+	var target = $(e.target);
+	if ( target.attr("class").indexOf("frameworklink") != -1 ) {
+		e.stopPropagation();
+		e.preventDefault();
+		framework.load(target.attr("href"));
+	}
+}, true);
+
 framework.load = function(page)
 {
-	$(".maincontent").load("pages/"+page);
+	$(document.body).animate({scrollTop: 0}, 500, function()
+	{
+		$(".maincontent").load("pages/"+page,function()
+		{
+		});
+	});
 }
 
 $(window).load(function()
